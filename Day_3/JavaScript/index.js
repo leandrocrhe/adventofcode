020209fc977b31067674f47afea36875bbebb585
leandrocrhe <108886070+leandrocrhe@users.noreply.github.com>
2023-07-07 16:04:19 -0400

    Day_3 - Segunda parte terminada modified:   Day_3/JavaScript/index.js

--- Day_3/JavaScript/index.js
+++ Day_3/JavaScript/index.js
@@ -9,28 +9,33 @@ fs.readFile('file_input.txt', 'utf-8', (err, data) => {
     const rucksacks = data.split('\n');
     
     const totalPriorities = calculateTotalPriorities(rucksacks);
+    const totalGroup = calculateTotalGroup(rucksacks);
+
     console.log(totalPriorities);
+    console.log(totalGroup);
   });
 
 
+  const lowercasePriorities = {};
+  const uppercasePriorities = {};
+  
+  // Crear objeto lowercasePriorities de forma dinámica
+  let lowercasePrioritiesArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97));
+  lowercasePrioritiesArray.reduce((obj, char, index) => {
+    obj[char] = index + 1;
+    return obj;
+  }, lowercasePriorities);
+  
+  // Crear objeto uppercasePriorities de forma dinámica
+  let uppercasePrioritiesArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
+  uppercasePrioritiesArray.reduce((obj, char, index) => {
+    obj[char] = index + 27;
+    return obj;
+  }, uppercasePriorities);
+  
+
+
 // --- Primera parte ---
-const lowercasePriorities = {};
-const uppercasePriorities = {};
-
-// Crear objeto lowercasePriorities de forma dinámica
-let lowercasePrioritiesArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97));
-lowercasePrioritiesArray.reduce((obj, char, index) => {
-  obj[char] = index + 1;
-  return obj;
-}, lowercasePriorities);
-
-// Crear objeto uppercasePriorities de forma dinámica
-let uppercasePrioritiesArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
-uppercasePrioritiesArray.reduce((obj, char, index) => {
-  obj[char] = index + 27;
-  return obj;
-}, uppercasePriorities);
-
 function getPriority(character) {
   if (lowercasePriorities.hasOwnProperty(character)) {
     return lowercasePriorities[character];
@@ -59,3 +64,36 @@ function calculateTotalPriorities(rucksacks) {
 };
 
 
+// --- Segunda parte ---
+function getPriority(character) {
+  return uppercasePriorities[character] || lowercasePriorities[character];
+}
+
+function findCommonCharacter(lines) {
+  const [line1, line2, line3] = lines;
+  const commonCharacters = [];
+
+  for (let char of line1) {
+    if (line2.includes(char)) {
+      commonCharacters.push(char);
+    }
+  }
+
+  for (let char of commonCharacters) {
+    if (line3.includes(char)) {
+      return getPriority(char);
+    }
+  }
+
+  return 0;
+}
+
+function calculateTotalGroup(rucksacks) {
+  let totalPriorities = 0;
+
+  const result = rucksacks.map((str) => str.trim());
+  for (let i = 0; i < result.length; i += 3) {
+    totalPriorities += findCommonCharacter(result.slice(i, i + 3));
+  }
+  return totalPriorities;
+}
